c7e47b1bfcd5561f651cb75870890e94e42c7433
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-02-22 09:12:40 -0300

    Update da Consulta

--- Pontos CX Master (Campanha Danilla).sql
+++ Pontos CX Master (Campanha Danilla).sql
@@ -1,8 +1,7 @@
-(Select ped.codusur,ped.numped,ped.codprod,ped.codcli,ped.qt,pro.qtunitcx,pro.embalagem,
-        (TRUNC(ped.qt/pro.qtunitcx,0))*10 Pontos_CX
+(Select SUM((TRUNC(ped.qt/pro.qtunitcx,0))*10) Pontos_CX
     from pcpedi ped, pcprodut pro
     where CODFORNEC = 1658
     and ped.CODPROD = pro.CODPROD
     and ped.posicao like 'F'
-    --and ped.qt <> ped.qtseparada
+    --and ped.codusur = 174
     and DATA BETWEEN '30-JAN-2023' AND '30-abr-2023')
