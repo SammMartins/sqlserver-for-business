61094abf8921776d96d88a87b47902195c2d280c
SAMMUEL GUSMÃO MARTINS <118392533+SammMartins@users.noreply.github.com>
2023-03-11 08:10:56 -0300

    Finalizando a consulta

--- Campanha Docile.sql
+++ Campanha Docile.sql
@@ -1,35 +1,72 @@
 Select rca.codsupervisor,rca.codusur || ' ' ||rca.nome RCA,
 ------------------------------------------------------------------------------------------------------------------
+(select vlvendaprev from pcmeta 
+    where codigo = 120423 
+        and data = '1-mar-2023' 
+        and codusur = rca.codusur) MetaF,
+------------------------------------------------------------------------------------------------------------------
 --Atingir meta Sell-out Mar./Abr. de R$ 190.000,00
 (SELECT SUM(mov.QT*mov.PUNIT) faturamento 
     from pcmov mov
-    JOIN PCPRODUT prod ON mov.CODPROD = prod.CODPROD
+        JOIN PCPRODUT prod ON mov.CODPROD = prod.CODPROD
     WHERE mov.dtmov BETWEEN '01-mar-2023' and '30-abr-2023'
-    and mov.codusur = rca.codusur
-    --AND mov.codoper = 'S'
-    and prod.codfornec = 1627) - (select nvl(sum(mov.qt * mov.punit),0)
+        and mov.codusur = rca.codusur
+        AND mov.codoper = 'S'
+        and prod.codfornec = 1627) - (select nvl(sum(mov.qt * mov.punit),0)
                                         from pcmov mov
-                                        join pcprodut B on mov.codprod = B.codprod
+                                            join pcprodut B on mov.codprod = B.codprod
                                         where mov.codcli = rca.codusur
-                                        and mov.dtmov between '01-mar-2023' and '30-abr-2023'
-                                        and mov.codoper = 'ED') Faturamento,
+                                            and mov.dtmov between '01-mar-2023' and '30-abr-2023'
+                                            and mov.codoper = 'ED') Faturado,
 ------------------------------------------------------------------------------------------------------------------
---1.600 positivações somando os 2 meses
+(SELECT SUM(mov.QT*mov.PUNIT) faturamento 
+    from pcmov mov
+        JOIN PCPRODUT prod ON mov.CODPROD = prod.CODPROD
+    WHERE mov.dtmov BETWEEN '01-mar-2023' and '30-abr-2023'
+        and mov.codusur = rca.codusur
+        AND mov.codoper = 'S'
+        and prod.codfornec = 1627) - (select nvl(sum(mov.qt * mov.punit),0)
+                                        from pcmov mov
+                                            join pcprodut B on mov.codprod = B.codprod
+                                        where mov.codcli = rca.codusur
+                                            and mov.dtmov between '01-mar-2023' and '30-abr-2023'
+                                            and mov.codoper = 'ED')-(select vlvendaprev from pcmeta 
+                                                                        where codigo = 120423 
+                                                                            and data = '1-mar-2023' 
+                                                                            and codusur = rca.codusur)  GAP_f,
+------------------------------------------------------------------------------------------------------------------
+(select cliposprev from pcmeta 
+    where codigo = 1627
+        and data = '1-mar-2023'
+        and codusur = rca.codusur) MetaP,
+------------------------------------------------------------------------------------------------------------------
+--1.600 positivaÃ§Ãµes somando os 2 meses
 (select count(distinct b.codcli)
-from pcmov b 
-join pcprodut d on b.codprod = d.codprod
-where b.codfornec = 1627
-AND B.CODOPER = 'S'
-and b.codusur = rca.codusur
-and b.dtmov between '01-mar-2023' and '30-abr-2023') Positivado
+    from pcmov b 
+        join pcprodut d on b.codprod = d.codprod
+    where b.codfornec = 1627
+        AND B.CODOPER = 'S'
+        and b.codusur = rca.codusur
+        and b.dtmov between '01-mar-2023' and '30-abr-2023') Positivado,
+------------------------------------------------------------------------------------------------------------------
+(select cliposprev from pcmeta 
+    where codigo = 1627
+        and data = '1-mar-2023'
+        and codusur = rca.codusur) - (select count(distinct b.codcli)
+                                        from pcmov b 
+                                            join pcprodut d on b.codprod = d.codprod
+                                        where b.codfornec = 1627
+                                            AND B.CODOPER = 'S'
+                                            and b.codusur = rca.codusur
+                                            and b.dtmov between '01-mar-2023' and '30-abr-2023')gap_P
 ------------------------------------------------------------------------------------------------------------------
 from pcusuari rca
-Where rca.DTULTVENDA > '01-mar-2023'
-and rca.codsupervisor in (2,8,9) and rca.codusur not in (2,149,160,148,145,150,152,168,174)
 
+Where rca.DTULTVENDA > '01-mar-2023'
+    and rca.codsupervisor in (2,8,9) 
+    and rca.codusur not in (2,149,160,148,145,150,152,168,174)
 
 order by rca
---Sell-inn de R$ 140.000,00 NO BIMESTRE
 
 
---select * from pcmov
+--select * from pcmeta where codigo in (120423,1627) and data = '1-mar-2023'
